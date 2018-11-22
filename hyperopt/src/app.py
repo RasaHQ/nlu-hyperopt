8d430b99c24593ebfe6fa551b489be23a69845e5
Alan Nichol <alan@golastmile.com>
2018-11-22 16:13:22 +0100

    add more hypers

--- hyperopt/src/app.py
+++ hyperopt/src/app.py
@@ -2,7 +2,7 @@ import math
 from hyperopt import fmin, tpe, hp, space_eval
 from hyperopt.mongoexp import MongoTrials
 
-def objective(x):
+def objective(args):
     from hyperopt import STATUS_OK
     from rasa_nlu.training_data import load_data
     from rasa_nlu.config import RasaNLUModelConfig
@@ -10,18 +10,21 @@ def objective(x):
     from rasa_nlu.evaluate import run_evaluation
     from rasa_nlu.model import Trainer, Interpreter, Metadata
 
+    epochs, max_df, max_ngram = args
     config_yml = """
 language: en
 pipeline:
 - name: "intent_featurizer_count_vectors"
 - name: "intent_classifier_tensorflow_embedding"
-  "epochs": {}
+  epochs: {}
 """.format(round(x))
     config = read_yaml(config_yml)
     config = RasaNLUModelConfig(config)
     trainer = Trainer(config)
     # temporary hack around nlu bug
     trainer.pipeline[1].epochs = round(x)
+    trainer.pipeline[0].max_df = max_df
+    trainer.pipeline[0].max_ngram = max_ngram
     training_data = load_data('/hyperopt/data/train.md')
     model = trainer.train(training_data)
     model_path = trainer.persist('/hyperopt/models')
@@ -30,7 +33,10 @@ pipeline:
     return {'loss': 1-intent_f1, 'status': STATUS_OK }
 
 print("starting process")
-space = hp.qloguniform('epochs', 0, 4, 2)
+space = [
+    hp.qloguniform('epochs', 0, 4, 2),
+    hp.loguniform('max_df', -10, 0),
+    hp.uniform('max_ngram', 1, 5)]
 trials = MongoTrials('mongo://mongodb:27017/foo_db/jobs', exp_key='exp3')
 best = fmin(objective, space, trials=trials, algo=tpe.suggest, max_evals=100)
 
