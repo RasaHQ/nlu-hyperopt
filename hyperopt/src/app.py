d2f02903ed33a78aaf79b5b8046817cd9705ef89
Alan Nichol <alan@golastmile.com>
2018-11-22 17:10:14 +0100

    change space

--- hyperopt/src/app.py
+++ hyperopt/src/app.py
@@ -37,7 +37,7 @@ print("starting process")
 space = {
     'epochs': hp.qloguniform('epochs', 0, 4, 2),
     'max_df': hp.loguniform('max_df', -10, 0),
-    'max_ngram': hp.uniform('max_ngram', 1, 5)
+    'max_ngram': hp.qloguniform('max_ngram', 0, 2, 1)
 }
 trials = MongoTrials('mongo://mongodb:27017/foo_db/jobs', exp_key='exp3')
 best = fmin(objective, space, trials=trials, algo=tpe.suggest, max_evals=100)
