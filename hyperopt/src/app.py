d106c3bb12b4be33e322e95f684d694c366671f9
Alan Nichol <alan@golastmile.com>
2018-11-23 17:24:57 +0100

    env vars for configuring stuff, read space from mounted file

--- hyperopt/src/app.py
+++ hyperopt/src/app.py
@@ -1,49 +1,69 @@
 import math
 from hyperopt import fmin, tpe, hp, space_eval
 from hyperopt.mongoexp import MongoTrials
+from space import search_space
+import os
 
 def objective(space):
+    """Objective function has to be picklable, hence
+    contain all the imports we need.
+    """
     from hyperopt import STATUS_OK
     from rasa_nlu.training_data import load_data
     from rasa_nlu.config import RasaNLUModelConfig
     from rasa_nlu.utils import read_yaml
     from rasa_nlu.evaluate import run_evaluation
     from rasa_nlu.model import Trainer, Interpreter, Metadata
+    import os
 
-    config_yml = """
-language: en
-pipeline:
-- name: "intent_featurizer_count_vectors"
-- name: "intent_classifier_tensorflow_embedding"
-  epochs: {}
-""".format(int(space['epochs']))
-    config = read_yaml(config_yml)
-    config = RasaNLUModelConfig(config)
-    trainer = Trainer(config)
-    # temporary hack around nlu bug
+    hyper_dir = os.environ.get("HYPEROPT_DIR", "./hyperopt")
+    data_dir = os.path.join(hyper_dir, "data")
     print(space)
-    trainer.pipeline[1].epochs = int(space['epochs'])
-    trainer.pipeline[0].max_df = float(space['max_df'])
-    trainer.pipeline[0].max_features = int(space['max_features'])
-    training_data = load_data('/hyperopt/data/train.md')
+
+    with open(os.path.join(data_dir, "template_config.yml")) as f:
+        config_yml = f.read().format(**space)
+        config = read_yaml(config_yml)
+        config = RasaNLUModelConfig(config)
+
+    trainer = Trainer(config)
+    training_data = load_data(os.path.join(data_dir, 'train.md'))
+    # wrap in train and eval in try/except in case
+    # hyperopt proposes invalid conbination of params
     try:
         model = trainer.train(training_data)
-        model_path = trainer.persist('/hyperopt/models')
-        evaluation = run_evaluation('/hyperopt/data/test.md', model_path, confmat_filename=None)
+        model_path = trainer.persist(os.path.join(hyper_dir, 'models/'))
+        evaluation = run_evaluation(os.path.join(data_dir, 'test.md'),
+                                    model_path)
         intent_f1 = evaluation['intent_evaluation']['f1_score']
-        print("f1 score: {}".format(intent_f1))
+        print("intent f1: {}".format(intent_f1))
         return {'loss': 1-intent_f1, 'status': STATUS_OK }
     except:
         return {'loss': 1, 'status': STATUS_OK }
 
-print("starting process")
-space = {
-    'epochs': hp.qloguniform('epochs', 0, 4, 2),
-    'max_df': hp.uniform('max_df', 0.01, 1.0),
-    'max_features': hp.qloguniform('max_features', 0, 9, 5)
-}
-trials = MongoTrials('mongo://mongodb:27017/foo_db/jobs', exp_key='exp8')
-best = fmin(objective, space, trials=trials, algo=tpe.suggest, max_evals=500)
-#best = fmin(objective, space, algo=tpe.suggest, max_evals=20)
-print(best)
-print(space_eval(space, best))
+
+if __name__ == "__main__":
+
+    exp_key = os.environ.get("EXP_KEY", "default")
+    mongo_database = os.environ.get("MONGO_DATABASE")
+    max_evals = int(os.environ.get("MAX_EVALS", 100))
+
+    print("starting up\n"
+          "running experiment : {}\n"
+          "max evaluations: {} ".format(exp_key, max_evals))
+
+    print("search space:")
+    print(search_space)
+
+    if mongo_database:
+        print("running async via mongo")
+        url = 'mongo://mongodb:27017/{}/jobs'.format(mongo_database)
+        trials = MongoTrials(url, exp_key=exp_key)
+    else:
+        print("running in memory")
+        trials = None
+
+    best = fmin(objective, search_space, trials=trials, algo=tpe.suggest, max_evals=max_evals)
+
+    print("search complete!")
+    print(best)
+    print(space_eval(space, best))
